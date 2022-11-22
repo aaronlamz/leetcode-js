f98cfafc90d342ab699276c6ae665f91e8102ed2
aaronlamz <aaronlamz2022@gmail.com>
2022-11-22 20:34:07 +0800

    update --docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,24 +5,32 @@ export default {
   // Theme related configurations.
   themeConfig: {
     logo: '/logo.png',
-
-    head: [['link', { rel: 'icon', href: '/leetcode-js/favicon.ico' }]],
+    head: [
+      ['link', { rel: 'icon', href: '/leetcode-js/favicon.ico'}]
+    ],
     nav: [
       { text: 'Introduction', link: '/md/leetcode/intro.md'},
       { text: 'Github', link: 'https://github.com/aaronlamz/leetcode-js'}
     ],
     sidebar: {
-      '/md/leetcode/':[
+      '/md/':[
         {
-          text: 'Algorithm',
+          text: 'Leetcode',
           collapsible: true,
           items: [
-            { text: 'Introduction', link: '/md/leetcode/intro' },
+            { text: '简介', link: '/md/leetcode/intro' },
             { text: '1.Two Sum', link: '/md/leetcode/01/index' },
             { text: '2.Add Tow Numbers', link: '/md/leetcode/02/index' },
           ]
+        },
+        {
+          text: '数据结构与算法',
+          collapsible: true,
+          items: [
+            { text: '简介', link: '/md/data-structure/intro' },
+          ]
         }
-      ]
+      ],
     }
   }
 }
