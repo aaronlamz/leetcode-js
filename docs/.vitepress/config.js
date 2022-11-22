ccd60d32a7b56726ebab8997142ece54c7c20675
aaronlamz <aaronlamz2022@gmail.com>
2022-11-22 20:45:37 +0800

    --docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,16 +18,20 @@ export default {
           text: 'Leetcode',
           collapsible: true,
           items: [
-            { text: '简介', link: '/md/leetcode/intro' },
-            { text: '1、两数之和', link: '/md/leetcode/01/index' },
-            { text: '2、两数相加', link: '/md/leetcode/02/index' },
+            { text: '简介', link: '/md/leetcode/intro'},
+            { text: '1. 两数之和', link: '/md/leetcode/01/'},
+            { text: '2. 两数相加', link: '/md/leetcode/02/'},
+            { text: '3. 无重复字符的最长子串', link: '/md/leetcode/03/'},
+            { text: '4. 寻找两个正序数组的中位数', link: '/md/leetcode/04/'},
+            { text: '5. 最长回文子串', link: '/md/leetcode/05/'},
+            { text: '6. Z 字形变换', link: '/md/leetcode/06/'},
           ]
         },
         {
           text: '数据结构与算法',
           collapsible: true,
           items: [
-            { text: '简介', link: '/md/data-structure/intro' },
+            { text: '简介', link: '/md/data-structure/intro'},
           ]
         }
       ],
